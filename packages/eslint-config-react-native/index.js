b47f251d67e153c5c3600f414fdba0398cc677ce
Michael Borde <michael.borde@eove.fr>
2022-06-27 14:21:36 +0200

    feat(eslint-config-react-native): linting about curly braces presence

--- packages/eslint-config-react-native/index.js
+++ packages/eslint-config-react-native/index.js
@@ -6,6 +6,10 @@ module.exports = {
     "react/no-did-update-set-state": "off",
     "react/no-did-mount-set-state": "off",
     "react/prop-types": "error",
+    "react/jsx-curly-brace-presence": [
+      "error",
+      { props: "never", children: "never", propElementValues: "always" },
+    ],
     "react-native/no-unused-styles": "error",
     "react-native/no-inline-styles": "error",
     "prettier/prettier": "off",
